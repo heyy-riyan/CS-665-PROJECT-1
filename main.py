c6746b1b567a988ec86cd664143aa9a2063296f9
Mohammed Abdul Wali Farooqui <riyanwali099@gmail.com>
2025-04-12 21:00:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,35 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = params['proud_uri']
 
 db = SQLAlchemy(app)
+
+class Medicines(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(500), nullable=False)
+    medicines= db.Column(db.String(500), nullable=False)
+    products = db.Column(db.String(500), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    mid = db.Column(db.String(120), nullable=False)
+
+class Posts(db.Model):
+    mid = db.Column(db.Integer, primary_key=True)
+    medical_name = db.Column(db.String(80), nullable=False)
+    owner_name = db.Column(db.String(200), nullable=False)
+    phone_no = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+
+class Addmp(db.Model):
+    sno = db.Column(db.Integer,  primary_key=True)
+    medicine = db.Column(db.String, nullable=False)
+
+class Addpd(db.Model):
+    sno = db.Column(db.Integer,  primary_key=True)
+    product = db.Column(db.String, nullable=False)
+
+
+class Logs(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    mid = db.Column(db.String, nullable=True)
+    action = db.Column(db.String(30), nullable=False)
+    date = db.Column(db.String(100), nullable=False)
