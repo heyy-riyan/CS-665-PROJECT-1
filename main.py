a48e0e6b9de8e33111b842d87912b7eaf685acd0
Mohammed Abdul Wali Farooqui <riyanwali099@gmail.com>
2025-04-12 20:44:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+rom flask import Flask, render_template, request, session, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+with open('config.json','r') as c:
+    params = json.load(c)["params"]
+
+
+local_server = True
+app = Flask(__name__)
+app.secret_key = 'super-secret-key'
+
+
+
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+
+
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['proud_uri']
+
+db = SQLAlchemy(app)
